470a6266202ff366c644348f6a375d4569228dc1
anangyoga <aharwinto@gmail.com>
2022-12-05 22:41:47 +0700

    feat: input field component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup></script>
+<script setup>
+import InputField from "./components/InputField.vue";
+</script>
 
 <template>
-  <h2>Lorem, ipsum dolor.</h2>
+  <h1>Convert Words</h1>
+  <main>
+    <InputField />
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  text-align: center;
+}
+main {
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+}
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,41 @@
+<script setup></script>
+
+<template>
+  <div class="container">
+    <p>150 Words</p>
+    <textarea name="fields" id="note" cols="60" rows="20"> </textarea>
+    <div class="buttons">
+      <button>Capitalize</button>
+      <button>lower case</button>
+      <button>UPPER CASE</button>
+      <button>Sentence case</button>
+      <button>Clear</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 25px;
+}
+
+.container p {
+  margin-left: 800px;
+}
+textarea {
+  border-radius: 15px;
+
+  width: 900px;
+  overflow: hidden;
+}
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+}
+
+.buttons button {
+  cursor: pointer;
+}
+</style>
