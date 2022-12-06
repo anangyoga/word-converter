f368a2d140fe87baf9f8b7b303d036ef8057002f
anangyoga <aharwinto@gmail.com>
2022-12-06 17:01:25 +0700

    feat: handle lower case

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,16 +1,25 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const input = ref("");
+
+const handleLowerCase = () => {
+  input.value = input.value.toLowerCase();
+};
+</script>
 
 <template>
   <div class="container">
     <p>150 Words</p>
-    <textarea name="fields" id="note" cols="60" rows="20"> </textarea>
+    <textarea v-model="input" name="fields" id="note" cols="60" rows="20"> </textarea>
     <div class="buttons">
       <button>Capitalize</button>
-      <button>lower case</button>
+      <button @click="handleLowerCase">lower case</button>
       <button>UPPER CASE</button>
       <button>Sentence case</button>
       <button>Clear</button>
     </div>
+    {{ input }}
   </div>
 </template>
 
